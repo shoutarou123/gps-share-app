300821e88addd977a8071e2a67ca098d6abbf6b9
shotaro <850123.a@gmail.com>
2025-03-24 21:53:02 +0900

    とりあえずtest修正

--- src/__tests__/AppComponent.spec.tsx
+++ src/__tests__/AppComponent.spec.tsx
@@ -1,9 +1,10 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import Home from "../compornents/home";
 
 describe("title", () => {
   it("should render title", () => {
-    render(<App />);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
+    render(<Home />);
+    expect(screen.getByText("home")).toBeInTheDocument();
   });
 });
