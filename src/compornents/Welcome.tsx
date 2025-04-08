6cd3923a33725dd8dd719643a8f6e1d76a523118
shotaro <850123.a@gmail.com>
2025-04-08 16:13:05 +0900

    css調整

--- src/compornents/Welcome.tsx
+++ src/compornents/Welcome.tsx
@@ -5,9 +5,14 @@ export const Welcome = () => {
   return (
 
     <>
-      <h1>Welcome!</h1>
-      <p>登録完了しました</p>
-      <a className='text-center hover:underline cursor-pointer hover:text-blue-600' onClick={() => navigate("/login")}>ログイン画面へ</a>
+      <div className="flex flex-col">
+        <div className="space-y-4 mx-auto">
+
+          <h1 className="text-2xl font-bold m-6">Welcome!</h1>
+          <p>登録完了しました</p>
+          <a className='text-center underline cursor-pointer text-blue-600' onClick={() => navigate("/login")}>ログイン画面へ</a>
+        </div>
+      </div>
 
     </>
   )
