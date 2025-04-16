8e53e8fb36af56dbe5613981a66656f558eec591
shotaro <850123.a@gmail.com>
2025-04-16 18:46:06 +0900

    Idをとる関数なので名称変更

--- lib/authUser.ts
+++ lib/authUser.ts
@@ -1,7 +1,7 @@
 import { supabase } from "../utils/supabase";
 
 
-export const GetUserData = async (): Promise<string | undefined> => {
+export const GetUserDataId = async (): Promise<string | undefined> => {
   const {data: {user}, error} = await supabase.auth.getUser();
   if(error) throw error;
   return user?.id;
